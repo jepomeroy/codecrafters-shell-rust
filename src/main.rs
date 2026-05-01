e6cf116b6181b32a015b2e0a5e4e6fb8837fb853
Jon Pomeroy <earl.pomeroy42@gmail.com>
2026-04-30 21:27:44 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
+    let mut input = String::new();
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => println!("{}: command not found", input.trim()),
+        Err(e) => println!("Error: {e}"),
+    }
 }
